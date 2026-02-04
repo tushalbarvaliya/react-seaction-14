9ed67fc2c07f59ff438a24f5f63e8dc7f876f400
tushalbarvaliya <tushal.barvaliya@seaflux.tech>
2026-02-04 09:45:11 +0530

    feat:add error catch in class components

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -12,8 +12,14 @@ class Users extends Component {
     };
   }
 
+  componentDidUpdate() {
+    
+    if (this.props.users.length === 0) {
+      throw new Error('No users provided!');
+    }
+  }
+
   toggleUsersHandler() {
-    // this.state.showUsers = false; // NOT!
     this.setState((curState) => {
       return { showUsers: !curState.showUsers };
     });
@@ -38,30 +44,5 @@ class Users extends Component {
     );
   }
 }
-
-// const Users = () => {
-//   const [showUsers, setShowUsers] = useState(true);
-
-//   const toggleUsersHandler = () => {
-//     setShowUsers((curState) => !curState);
-//   };
-
-//   const usersList = (
-//     <ul>
-//       {DUMMY_USERS.map((user) => (
-//         <User key={user.id} name={user.name} />
-//       ))}
-//     </ul>
-//   );
-
-//   return (
-//     <div className={classes.users}>
-//       <button onClick={toggleUsersHandler}>
-//         {showUsers ? 'Hide' : 'Show'} Users
-//       </button>
-//       {showUsers && usersList}
-//     </div>
-//   );
-// };
 
 export default Users;
